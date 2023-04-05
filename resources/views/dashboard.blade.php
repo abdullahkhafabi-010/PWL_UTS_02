fbe42719dc076e8064e5956ebd291951aa6282a0
= <=>
2023-04-05 13:15:50 +0700

    uts2

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<script>
+  alert('Selamat Datang');
+</script>
+
+@extends('layout.main')
+
+@section('isi')
+    
+<!-- Main content -->
+<section class="content">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="inner">
+            <h4 style="font-weight: bold">Selamat Datang</h4>
+
+            <h5 style="font-weight: bold">{!! $full_name !!}</h5>
+        </div>
+      </div>
+</section>
+  <!-- /.content-wrapper -->
+@endsection
